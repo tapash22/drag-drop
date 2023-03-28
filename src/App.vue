d2233fa8eb438196fe84dcf704facee9dbc31a00
tapash22 <tapasp263@gmail.com>
2023-03-28 22:10:29 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,77 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div id="App">
+    <div class="drop-zone" @drop="onDrop($event, 1)" @dragover.prevent @dragenter.prevent>
+      <div class="drag-el" v-for="item in listOne" :key="item.title" draggable @dragstart="startDrag($event, item)">
+        {{ item.title }}
+      </div>
+    </div>
+    <div class="drop-zone" @drop="onDrop($event, 2)" @dragover.prevent @dragenter.prevent>
+      <div class="drag-el" v-for="item in listTwo" :key="item.title" draggable  @dragstart="startDrag($event, item)">
+        {{ item.title }}
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      items: [{
+        id: 0,
+        title: 'Item A',
+        list: 1,
+      },
+      {
+        id: 1,
+        title: 'Item B',
+        list: 1,
+      },
+      {
+        id: 2,
+        title: 'Item C',
+        list: 2,
+      },
+      ],
+    }
+  },
+
+  computed: {
+    listOne() {
+      return this.items.filter((item) => item.list === 1)
+    },
+    listTwo() {
+      return this.items.filter((item) => item.list === 2)
+    },
+  },
+
+  methods: {
+    startDrag(evt, item) {
+      evt.dataTransfer.dropEffect = 'move'
+      evt.dataTransfer.effectAllowed = 'move'
+      evt.dataTransfer.setData('itemID', item.id)
+    },
+    onDrop(evt, list) {
+      const itemID = evt.dataTransfer.getData('itemID')
+      const item = this.items.find((item) => item.id == itemID)
+      item.list = list
+    },
+  },
+
+}
+</script>
+
+<style scoped>
+.drop-zone {
+  background-color: #eee;
+  margin-bottom: 10px;
+  padding: 10px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.drag-el {
+  background-color: #fff;
+  margin-bottom: 10px;
+  padding: 5px;
+  height: 20px;
 }
 </style>
